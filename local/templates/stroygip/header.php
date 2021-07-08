3cf7bfb1ccdc22a3a742c7daf05c0863cc88a5aa
arman3 <a.saparbekov@bk.ru>
2021-07-08 20:44:13 +0600

    counters to footer
    перенос счетчиков

--- local/templates/stroygip/header.php
+++ local/templates/stroygip/header.php
@@ -68,44 +68,6 @@ global $USER;
     <link href="<?= SITE_TEMPLATE_PATH ?>/ts/main.css" rel="stylesheet">
 </head>
 
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-    (function (m, e, t, r, i, k, a) {
-        m[i] = m[i] || function () {
-            (m[i].a = m[i].a || []).push(arguments);
-        };
-        m[i].l = 1 * new Date();
-        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
-    })
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    ym(61269028, "init", {
-        clickmap: true,
-        trackLinks: true,
-        accurateTrackBounce: true,
-        webvisor: true,
-        ecommerce: "dataLayer"
-    });
-</script>
-<noscript>
-    <div><img src="https://mc.yandex.ru/watch/61269028" style="position:absolute; left:-9999px;" alt=""/></div>
-</noscript>
-<!-- /Yandex.Metrika counter -->
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160628157-1"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-
-    gtag('js', new Date());
-
-    gtag('config', 'UA-160628157-1');
-</script>
-</head>
 <body>
 <div id="panel">
     <? $APPLICATION->ShowPanel(); ?>
