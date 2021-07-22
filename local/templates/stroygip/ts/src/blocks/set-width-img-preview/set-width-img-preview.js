a2d49abb24d2f1bbe33a9ca2c792db13724561cd
yury-ustinov <yury.inform@gmail.com>
2021-07-22 20:03:59 +0300

    Изменение картинок

--- local/templates/stroygip/ts/src/blocks/set-width-img-preview/set-width-img-preview.js
+++ local/templates/stroygip/ts/src/blocks/set-width-img-preview/set-width-img-preview.js
@@ -1,7 +1,7 @@
 export function setWidthImgPreview($preview) {
     let $price = $preview.querySelector('.product-cart__price');
     let $img = $preview.querySelector('.product-cart__img');
-    let padding = 15
+    let padding = 15;
 
     let previewWidth = parseInt(window.getComputedStyle($preview).width);
     let imgRight = parseInt(window.getComputedStyle($img).right);
@@ -9,4 +9,5 @@ export function setWidthImgPreview($preview) {
     let priceLeft = parseInt(window.getComputedStyle($price).left);
 
     $img.style.width = (previewWidth - imgRight - priceWidth - priceLeft - padding) + 'px';
+    $img.style.opacity = 1;
 }
