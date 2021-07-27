9263d1e1d087bdc5f21f63be4dffc354c25cbf69
arman3 <a.saparbekov@bk.ru>
2021-07-27 15:32:20 +0600

    правка пустой корзины

--- local/templates/stroygip/components/bitrix/sale.basket.basket/basket2/empty.php
+++ local/templates/stroygip/components/bitrix/sale.basket.basket/basket2/empty.php
@@ -1,6 +1,13 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 ?>
-<div class="basket-empty__inner">
-    <div class="title basket-empty__title">В корзине пока ничего нет</div>
-    <div class="basket-empty__text">Воспользуйтесь поиском, чтобы найти нужный товар или<a class="basket-empty__link" href="/catalog/">перейдите в каталог.</a></div>
+<div class="basket basket--empty">
+	<div class="basket__empty">
+		<svg class="basket__empty-icon">
+			<use xlink:href="/local/templates/stroygip/ts/images/icons/icons-sprite.svg#empty-box"></use>
+		</svg>
+		<div class="basket__empty-text-box">
+			<div class="basket__empty-text">Корзина пуста. Вы можете выбрать товары в</div>
+			<a class="basket__empty-text-link" href="/catalog/">&nbsp;каталоге</a>
+		</div>
+	</div>
 </div>
