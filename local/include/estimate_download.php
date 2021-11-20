3b9d4b48277e5c894c31adcb2067ac70daaeeedd
arman3 <a.saparbekov@bk.ru>
2021-11-20 20:12:56 +0600

    Update estimate_download.php

--- local/include/estimate_download.php
+++ local/include/estimate_download.php
@@ -41,13 +41,15 @@ if ($_REQUEST['estimate']) {
             $sheet->setCellValue('C' . $cnt, $item['PRICE']);
 			$cnt++;
         }
+		
+		$filename = "Смета ".$_REQUEST['order_id'];
 
         header ( "Expires: Mon, 1 Apr 1974 05:00:00 GMT" );
         header ( "Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT" );
         header ( "Cache-Control: no-cache, must-revalidate" );
         header ( "Pragma: no-cache" );
         header ( "Content-type: application/vnd.ms-excel" );
-        header ( "Content-Disposition: attachment; filename=estimate.xls" );
+        header ( "Content-Disposition: attachment; filename=".$filename.".xls" );
 
 
         $objWriter = new PHPExcel_Writer_Excel2007($xls);
@@ -102,12 +104,14 @@ if ($_REQUEST['estimate']) {
 			$cnt++;
         }
 
+		$filename = "Смета ".$_REQUEST['estimate_id'];
+
         header ( "Expires: Mon, 1 Apr 1974 05:00:00 GMT" );
         header ( "Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT" );
         header ( "Cache-Control: no-cache, must-revalidate" );
         header ( "Pragma: no-cache" );
         header ( "Content-type: application/vnd.ms-excel" );
-        header ( "Content-Disposition: attachment; filename=estimate.xls" );
+        header ( "Content-Disposition: attachment; filename=".$filename.".xls" );
 
 
         $objWriter = new PHPExcel_Writer_Excel2007($xls);
