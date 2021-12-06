2bb7c5481a6ad40fbe49fc1ebd1f6d171cf2fb46
arman3 <a.saparbekov@bk.ru>
2021-12-06 23:18:31 +0600

    Update main_search.php

--- local/include/main_search.php
+++ local/include/main_search.php
@@ -7,33 +7,67 @@ CModule::IncludeModule('iblock');
 CModule::IncludeModule("main");
 CModule::IncludeModule('search');
 
-print_r($_POST);
+//print_r($_POST);
+
+$request_body = file_get_contents('php://input');
+$data1 = json_decode($request_body);
+$user_query = trim($data1->request);
 
 $arRes = array();
 $arRes["products"] = array();
 
 
 
-if( isset($_POST["request"]) && strlen(trim($_POST["request"]))>=3 ){
-	$user_query = trim($_POST["request"]);
-	
+if( strlen(trim($user_query))>=3 ){
 	$module_id = "iblock";
 	$param1 = 'catalog';
 	$param2 = 1;
 	$obSearch = new CSearch($user_query, LANG, $module_id, false, $param1, $param2);
 	while($arSearchObj = $obSearch->GetNext()){
-		$arItem = array(
-			"img" => "https://2proraba.kg/upload/resize_cache/iblock/abd/400_400_1/abd01f963019125fbeeeec4e50063156.png",
-			"name" => $arSearchObj["TITLE"],
-			"link" => $arSearchObj["URL"],
-			"price" => "555",
-		);
 		
-		$arRes["products"][] = $arItem;
+		$res_item = \CIBlockElement::GetList(
+		   array(),
+		   array('IBLOCK_ID' => 1, '=AVAILABLE'=>'Y', '=ID'=>$arSearchObj["ITEM_ID"]),
+		   false,
+		   false,
+		   array('ID', 'NAME', 'IBLOCK_ID', 'PRICE_1', 'AVAILABLE', 'PREVIEW_PICTURE', 'DETAIL_PICTURE')
+		);
+		if($ar_res = $res_item->GetNext()){
+			
+			$arItem = array(
+				"id" => $arSearchObj["ITEM_ID"],
+				"img" => "",
+				"name" => $arSearchObj["TITLE"],
+				"link" => $arSearchObj["URL"],
+				"price" => intval($ar_res["PRICE_1"]),
+			);
+		
+			if( strlen($ar_res["PREVIEW_PICTURE"])>0 ){
+				$file = CFile::ResizeImageGet($ar_res['PREVIEW_PICTURE'], array('width'=>60, 'height'=>60), BX_RESIZE_IMAGE_PROPORTIONAL, true);
+				$arItem["img"] = $file["src"];
+			}
+			if( empty($arItem["img"]) && strlen($ar_res["DETAIL_PICTURE"])>0 ){
+				$file = CFile::ResizeImageGet($ar_res['DETAIL_PICTURE'], array('width'=>60, 'height'=>60), BX_RESIZE_IMAGE_PROPORTIONAL, true);
+				$arItem["img"] = $file["src"];
+			}
+			if( empty($arItem["img"]) ){
+				$arItem["img"] = "/local/templates/stroygip/img/no-image.png";
+			}
+			
+		}
+		
+		if( intval($arItem["price"])>0 ){
+			$arRes["products"][] = $arItem;	
+		}
+		
+		
+		if( count($arRes["products"])>= 20 ){
+			break;
+		}
 	}
 }
 
-//print_r(json_encode($arRes));
+print_r(json_encode($arRes));
 
 
 require $_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_after.php";
